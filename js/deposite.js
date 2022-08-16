89a8cc014cbea58fdad830c2a097d4b2572fad3c
SamitMahadi <samithossainmahadi@gmail.com>
2022-08-16 07:50:59 -0400

    5th

--- js/deposite.js
+++ js/deposite.js
@@ -9,11 +9,23 @@ document.getElementById('btn-deposite').addEventListener('click', function () {
     //step03 : get the deposit total//
     //for non-input( element other than input, textarea) use inner text to get that text///
     const depositeTotalElement = document.getElementById('deposite-total');
-    const previousdepositeTotal = depositeTotalElement.innerText;
+    const previousdepositeTotalstring = depositeTotalElement.innerText;
+    const previousdepositeTotal = parseFloat(previousdepositeTotalstring);
 
+    //   step04 add numbers to total deposite//
     const currentdepositeTotal = previousdepositeTotal + newdepositeAmount;
 
     depositeTotalElement.innerText = currentdepositeTotal;
+
+
+    //step 05 :get balance current total//
+    const balancetotalelment = document.getElementById('balance-total');
+    const previousbalancetotalstring = balancetotalelment.innerText;
+    const previousbalancetotal = parseFloat(previousbalancetotalstring);
+    //step06 : calculate total balance//
+    const currentbalancetotal = previousbalancetotal + newdepositeAmount;
+    //set the balance total//
+    balancetotalelment.innerText = currentbalancetotal;
 
     //step 07: clear the deposite field//
     userDiposite.value = '';
